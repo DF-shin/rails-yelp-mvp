375e5707609561680afa588def9009e847a93124
declan schindler <declanschindler0929@live.com>
2024-02-28 13:05:34 -0500

    Rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+cat = %w[chinese italian japanese french belgian]
+rate = (0..5).to_a
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Food.ethnic_category,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: cat.sample
+  )
+  5.times do
+    review = Review.new(
+      rating: rate.sample,
+      content: Faker::Quote.famous_last_words
+    )
+    review.restaurant = restaurant
+    review.save!
+  end
+end
+
+puts 'Creating restaurants! With Reviews!'
